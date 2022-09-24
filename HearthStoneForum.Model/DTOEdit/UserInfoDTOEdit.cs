bc497984859c7456ddf77ff6e8b1f3db630d589d
Yu-Core <3105136125@qq.com>
2022-09-25 01:41:33 +0800

    完成修改资料

--- HearthStoneForum.Model/DTOEdit/UserInfoDTOEdit.cs
+++ HearthStoneForum.Model/DTOEdit/UserInfoDTOEdit.cs
@@ -1,6 +1,7 @@
 ﻿using SqlSugar;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace HearthStoneForum.Model.DTOEdit
 {
     public class UserInfoDTOEdit : BaseId
     {
+        [Required(ErrorMessage = "名字不能为空")]
+        [MaxLength(16, ErrorMessage = "名字长度必须在3到20之间")]
+        [MinLength(4, ErrorMessage = "名字长度必须在3到20之间")]
         public string? Name { get; set; }
         public string? Portrait { get; set; }
         public Sex Sex { get; set; }
+        [Required(ErrorMessage = "邮箱不能为空")]
+        [EmailAddress(ErrorMessage = "邮箱地址无效")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "手机号码不能为空")]
+        [Phone(ErrorMessage = "手机号码无效")]
         public string? Phone { get; set; }
     }
 }
--- HearthStoneForum.Model/DTORest/UserInfoDTOLogin.cs
+++ HearthStoneForum.Model/DTORest/UserInfoDTOLogin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,13 @@ namespace HearthStoneForum.Model.DTORest
 {
     public class UserInfoDTOLogin
     {
+        [Required(ErrorMessage = "用户名不能为空")]
+        [MaxLength(16, ErrorMessage = "账号长度必须在3到20之间")]
+        [MinLength(4, ErrorMessage = "账号长度必须在3到20之间")]
         public string? UserName { get; set; }
+        [Required(ErrorMessage = "密码不能为空")]
+        [MaxLength(16, ErrorMessage = "密码长度必须在5到20之间")]
+        [MinLength(4, ErrorMessage = "密码长度必须在5到20之间")]
         public string? Password { get; set; }
     }
 }
